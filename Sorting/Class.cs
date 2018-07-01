e468bfdf5d2f10fec9f03ed9e3428fbdf531c030
theeateroffood <theeateroffood@gmail.com>
2018-07-01 20:02:00 +1000

    wOO

---
+++ Sorting/Class.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Sorting
+{
+    public class Class
+    {
+        public List<int> ImportCSV(string CSVPath)
+        {
+            List<int> CSV_Lines = new List<int>();
+            string line;
+
+            StreamReader CSV = new StreamReader(CSVPath);
+            while ((line = CSV.ReadLine()) != null)
+            {
+                try
+                {
+                    CSV_Lines.Add(Convert.ToInt32(line));
+                }
+                catch (Exception) { }
+            }
+
+            return CSV_Lines;
+        }
+
+        public void PrintList(Array CSV_Lines)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            foreach (var i in CSV_Lines)
+            {
+
+                Console.WriteLine(i);
+            }
+            watch.Stop();
+
+            Console.WriteLine(watch.ElapsedMilliseconds);
+        }
+
+        public int InsertionSort(int[] array)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+            int j;
+            int temp;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                j = i;
+                while (j > 0 && array[j] < array[j - 1])
+                {
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    j--;
+                }
+            }
+
+            watch.Stop();
+
+            return unchecked((int)watch.ElapsedMilliseconds);
+        }
+
+        public int LinearSearch(int[] list, int item)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            int value = -1;
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i] == item)
+                {
+                    value = i;
+                    break;
+                }
+            }
+            watch.Stop();
+
+            return unchecked((int)watch.ElapsedMilliseconds);
+            }
+
+        public int ShellSort(int[] array)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            int n = array.Length;
+            int gap = n / 2;
+            int temp;
+
+            while (gap > 0)
+            {
+                for (int i = 0; i + gap < n; i++)
+                {
+                    int j = i + gap;
+                    temp = array[j];
+
+                    while (j - gap >= 0 && temp < array[j - gap])
+                    {
+                        array[j] = array[j - gap];
+                        j = j - gap;
+                    }
+
+                    array[j] = temp;
+                }
+
+                gap = gap / 2;
+            }
+
+            watch.Stop();
+            
+            return unchecked((int)watch.ElapsedMilliseconds);
+
+        }
+
+        public int BinarySearchIterative(int[] inputArray, int key)
+        {
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid])
+                {
+                    return ++mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            watch.Stop();
+
+            return unchecked((int)watch.ElapsedMilliseconds);
+        }
+    }
+}
--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -10,6 +10,39 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
+            Class bleh = new Class();
+            List<int> CSV_Lines = bleh.ImportCSV("unsorted_numbers.csv");
+            int[] arr = CSV_Lines.ToArray();
+
+            Console.WriteLine("Insertion time : " + bleh.InsertionSort(arr) + " Miliseconds");
+            arr = CSV_Lines.ToArray();
+            Console.WriteLine("Shellsort time : " + bleh.ShellSort(arr) + " Miliseconds");
+
+            Console.WriteLine("---------------------------------------------------------------");
+
+            Console.WriteLine("Linear Seach 1500 Time : " + bleh.LinearSearch(arr, 1500));
+            Console.WriteLine("Linear Seach 3000 Time : " + bleh.LinearSearch(arr, 3000));
+            Console.WriteLine("Linear Seach 4500 Time : " + bleh.LinearSearch(arr, 4500));
+            Console.WriteLine("Linear Seach 6000 Time : " + bleh.LinearSearch(arr, 6000));
+            Console.WriteLine("Linear Seach 7500 Time : " + bleh.LinearSearch(arr, 7500));
+            Console.WriteLine("Linear Seach 9000 Time : " + bleh.LinearSearch(arr, 9000));
+            Console.WriteLine("Linear Seach 10500 Time : " + bleh.LinearSearch(arr, 10500));
+            Console.WriteLine("Linear Seach 12000 Time : " + bleh.LinearSearch(arr, 12000));
+            Console.WriteLine("Linear Seach 13500 Time : " + bleh.LinearSearch(arr, 13500));
+            Console.WriteLine("Linear Seach 15000 Time : " + bleh.LinearSearch(arr, 15000));
+            Console.WriteLine();
+            Console.WriteLine("Binary Seach 1500 Time : " + bleh.BinarySearchIterative(arr, 1500));
+            Console.WriteLine("Binary Seach 3000 Time : " + bleh.BinarySearchIterative(arr, 3000));
+            Console.WriteLine("Binary Seach 4500 Time : " + bleh.BinarySearchIterative(arr, 4500));
+            Console.WriteLine("Binary Seach 6000 Time : " + bleh.BinarySearchIterative(arr, 6000));
+            Console.WriteLine("Binary Seach 7500 Time : " + bleh.BinarySearchIterative(arr, 7500));
+            Console.WriteLine("Binary Seach 9000 Time : " + bleh.BinarySearchIterative(arr, 9000));
+            Console.WriteLine("Binary Seach 10500 Time : " + bleh.BinarySearchIterative(arr, 10500));
+            Console.WriteLine("Binary Seach 12000 Time : " + bleh.BinarySearchIterative(arr, 12000));
+            Console.WriteLine("Binary Seach 13500 Time : " + bleh.BinarySearchIterative(arr, 13500));
+            Console.WriteLine("Binary Seach 15000 Time : " + bleh.BinarySearchIterative(arr, 15000));
+
+            Console.ReadLine();
         }
     }
 }
